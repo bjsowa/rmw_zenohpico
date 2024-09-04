667d7ea8a3a8fcf49b0b3ff88781d30d5b95f3ce
Błażej Sowa <bsowa123@gmail.com>
2024-09-04 10:50:43 +0200

    Add get_current_timestamp function

---
+++ rmw_zenohpico_c/src/detail/time.c
@@ -0,0 +1,15 @@
+#include "./time.h"
+
+#include "zenoh-pico.h"
+
+rmw_ret_t rmw_zp_get_current_timestamp(int64_t *timestamp) {
+  zp_time_since_epoch time_since_epoch;
+  if (zp_get_time_since_epoch(&time_since_epoch) < 0) {
+    RMW_SET_ERROR_MSG("Zenoh-pico port does not support zp_get_time_since_epoch");
+    return RMW_RET_ERROR;
+  }
+
+  *timestamp = (int64_t)time_since_epoch.secs * 1000000000ll + (int64_t)time_since_epoch.nanos;
+
+  return RMW_RET_OK;
+}
---
+++ rmw_zenohpico_c/src/detail/time.h
@@ -0,0 +1,8 @@
+#ifndef RMW_ZENOHPICO_DETAIL__TIME_H_
+#define RMW_ZENOHPICO_DETAIL__TIME_H_
+
+#include "rmw/ret_types.h"
+
+rmw_ret_t rmw_zp_get_current_timestamp(int64_t *timestamp);
+
+#endif
